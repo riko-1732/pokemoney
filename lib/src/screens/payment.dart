2a1fd1396890f25c1101936164df8a892bda75bc
riko-1732 <142651005+riko-1732@users.noreply.github.com>
2025-10-29 00:27:32 +0900

    add category choice(payment)

--- lib/src/screens/payment.dart
+++ lib/src/screens/payment.dart
@@ -12,6 +12,8 @@ class PaymentPage extends StatefulWidget {
 
 class _PaymentPageState extends State<PaymentPage> {
   DateTime selectedDate = DateTime.now();
+  int selectValue = 0;
+  String result = "";
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +30,61 @@ class _PaymentPageState extends State<PaymentPage> {
             TextField(
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            ),
+            Wrap(
+              spacing: 10,
+              children: [
+                ChoiceChip(
+                  label: Text("食事"),
+                  selected: selectValue == 1,
+                  onSelected: (bool isSelected) {
+                    setState(() {
+                      selectValue = isSelected ? 1 : 0;
+                      result = selectValue.toString();
+                    });
+                  },
+                ),
+                ChoiceChip(
+                  label: Text("お菓子"),
+                  selected: selectValue == 2,
+                  onSelected: (bool isSelected) {
+                    setState(() {
+                      selectValue = isSelected ? 2 : 0;
+                      result = selectValue.toString();
+                    });
+                  },
+                ),
+                ChoiceChip(
+                  label: Text("ライブ代"),
+                  selected: selectValue == 3,
+                  onSelected: (bool isSelected) {
+                    setState(() {
+                      selectValue = isSelected ? 3 : 0;
+                      result = selectValue.toString();
+                    });
+                  },
+                ),
+                ChoiceChip(
+                  label: Text("交通費"),
+                  selected: selectValue == 4,
+                  onSelected: (bool isSelected) {
+                    setState(() {
+                      selectValue = isSelected ? 4 : 0;
+                      result = selectValue.toString();
+                    });
+                  },
+                ),
+                ChoiceChip(
+                  label: Text("その他"),
+                  selected: selectValue == 5,
+                  onSelected: (bool isSelected) {
+                    setState(() {
+                      selectValue = isSelected ? 5 : 0;
+                      result = selectValue.toString();
+                    });
+                  },
+                ),
+              ],
             ),
             Text(
               '日付: ${selectedDate.year}/${selectedDate.month}/${selectedDate.day}',
