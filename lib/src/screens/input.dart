fa8d9876fc9ee1e41a86a482ae9baac6cc5be2d6
riko-1732 <142651005+riko-1732@users.noreply.github.com>
2025-10-28 22:39:15 +0900

    add calendar

--- lib/src/screens/input.dart
+++ lib/src/screens/input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key, required this.title});
@@ -10,37 +11,31 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('入力'),
+        bottom: const TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.trending_up), text: '収入'),
+            Tab(icon: Icon(Icons.trending_down), text: '支出'),
+          ],
+          indicatorColor: Colors.white,
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('inputpage'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextField(
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
